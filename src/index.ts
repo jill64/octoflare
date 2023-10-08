04bea7dc15e7d1af2b234e7d0f41b802c52518de
jill64 <intents.turrets0h@icloud.com>
2023-10-08 21:31:32 +0900

    feat: handler type export

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 export { octoflare } from './octoflare.js'
+export type { OctoflareHandler } from './types/OctoflareHandler.js'
--- src/types/OctoflareHandler.ts
+++ src/types/OctoflareHandler.ts
@@ -2,9 +2,10 @@ import { WebhookEvent } from '@octokit/webhooks-types'
 import { App } from 'octokit'
 import { InternalEnv } from './InternalEnv.js'
 
-export type OctoflareHandler<Env extends InternalEnv> = (context: {
-  request: Request
-  env: Env
-  app: App
-  payload: WebhookEvent
-}) => Promise<Response> | Response
+export type OctoflareHandler<Env extends InternalEnv = InternalEnv> =
+  (context: {
+    request: Request
+    env: Env
+    app: App
+    payload: WebhookEvent
+  }) => Promise<Response> | Response
