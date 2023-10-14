85f91e3c179bde5f5a7aa11e395a2406f0defe8f
jill64 <intents.turrets0h@icloud.com>
2023-10-14 21:10:17 +0900

    fix: exports

--- src/action/types/index.ts
+++ src/action/types/index.ts
@@ -1,4 +1,3 @@
 export type { ChecksOutput } from '../../types/ChecksOutput.js'
 export type { ActionHandler } from './ActionHandler.js'
 export type { ActionOctokit } from './ActionOctokit.js'
-export type { ActionResult } from './ActionResult.js'
