263a37368e898e5d2d8f0e775aa58b5671694d52
jill64 <intents.turrets0h@icloud.com>
2023-10-17 21:43:33 +0900

    fix: env types

--- src/types/OctoflareEnv.ts
+++ src/types/OctoflareEnv.ts
@@ -2,4 +2,6 @@ export type OctoflareEnv = {
   OCTOFLARE_APP_ID: string
   OCTOFLARE_PRIVATE_KEY_PKCS8: string
   OCTOFLARE_WEBHOOK_SECRET: string
+  OCTOFLARE_APP_OWNER: string
+  OCTOFLARE_APP_REPO: string
 }
