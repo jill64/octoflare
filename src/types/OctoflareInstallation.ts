9bebbe69e64e0d466495fb62a5ea0a11dff3c4b6
jill64 <intents.turrets0h@icloud.com>
2023-12-19 17:19:02 +0900

    add docs

--- src/types/OctoflareInstallation.ts
+++ src/types/OctoflareInstallation.ts
@@ -7,16 +7,39 @@ import { OctoflarePayloadData } from './OctoflarePayloadData.js'
 import { UpdateCheckRun } from './UpdateCheckRun.js'
 
 export type OctoflareInstallation<Data extends OctoflarePayloadData> = {
+  /**
+   * Octokit instance with installation access token.
+   */
   kit: Octokit
   createCheckRun: (
     params: NonNullable<Parameters<Octokit['rest']['checks']['create']>[0]>
   ) => Promise<{
+    /**
+     * Dispatch workflow run.
+     * It inherits `owner`, `repo`, `check_run_id` of `createCheckRun`.
+     */
     dispatchWorkflow: DispatchWorkflow<Data>
+
+    /**
+     * Update check run status as `complete`.
+     * It inherits `owner`, `repo`, `check_run_id` of `createCheckRun`.
+     */
     completeCheckRun: CompleteCheckRun
+
+    /**
+     * Update check run status.
+     * It inherits `owner`, `repo`, `check_run_id` of `createCheckRun`.
+     */
     updateCheckRun: UpdateCheckRun
   }>
+
+  /**
+   * Dispatch workflow run without check_run_id.
+   * If a workflow is started with this function, the automatic check-run control in the action is disabled.
+   */
   startWorkflow: (
     payload: Omit<OctoflarePayload<Data>, 'token' | 'app_token'>
   ) => Promise<void>
+
   getFile: InstallationGetFile
 }
